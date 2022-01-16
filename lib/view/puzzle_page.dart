a9f979c1803866923f12b020f0f2e17f3f488fdc
MayuriXx <martho.evan@gmail.com>
2022-01-16 15:59:23 +0100

    feat(shark): add shark animation for rotate screen

--- lib/view/puzzle_page.dart
+++ lib/view/puzzle_page.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:puzzle/bloc/bloc.dart';
 import 'package:puzzle/view/widgets/widgets.dart';
+import 'package:shake/shake.dart';
 
 /// Main page of the application.
 /// Contains the puzzle and the controls.
-class PuzzlePage extends StatelessWidget {
+class PuzzlePage extends StatefulWidget {
   const PuzzlePage({Key? key}) : super(key: key);
+
+  @override
+  State<PuzzlePage> createState() => _PuzzlePageState();
+}
+
+class _PuzzlePageState extends State<PuzzlePage> {
+  late ShakeDetector detector;
+
+  @override
+  void initState() {
+    super.initState();
+    detector = ShakeDetector.autoStart(onPhoneShake: () {
+      if (MediaQuery.of(context).orientation == Orientation.portrait) {
+        // is portrait
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.landscapeLeft,
+          DeviceOrientation.landscapeRight,
+        ]);
+      } else {
+        // is landscape
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitDown,
+          DeviceOrientation.portraitUp,
+        ]);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    detector.stopListening();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
