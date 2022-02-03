f2461d78cbdd577deaeba21351186cbd629864a2
nathanvandeperre97 <nathanvandeperre@gmail.com>
2022-02-03 12:01:33 +0100

    fix tests

--- test/puzzle_state_test.dart
+++ test/puzzle_state_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:puzzle/bloc/bloc.dart';
 import 'package:puzzle/models/models.dart';
+import 'package:puzzle/services/audio_service.dart';
 
 main() {
+  GetIt.I.registerSingleton(AudioService());
+
   test('Puzzle state should generate data on the fly', () {
     // GIVEN
     const int size = 3;
