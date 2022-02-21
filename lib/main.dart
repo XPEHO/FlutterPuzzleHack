c66cc47f5d5e6f0eb00696f2a202352fe2376e75
Martho Evan <martho.evan@gmail.com>
2022-02-21 17:56:35 +0100

    Feat/UI leaderboard (#35)

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:puzzle/app/app.dart';
 import 'package:puzzle/dependency_injection.dart';
-import 'package:puzzle/firebase_options.dart';
 import 'package:puzzle/services/shared.dart';
+
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/view/homepage/homepage.dart
+++ lib/view/homepage/homepage.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:puzzle/providers/leaderboard_provider.dart';
 import 'package:puzzle/services/shared.dart';
 import 'package:puzzle/view/homepage/widgets/menu_button.dart';
 import 'package:puzzle/view/view.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePage extends StatefulWidget {
   static const String route = "/homePage";
