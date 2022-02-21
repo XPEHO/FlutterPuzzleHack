e757fd8891faec0eb222a096323e2a2d281d1465
nathanvandeperre97 <nathanvandeperre@gmail.com>
2022-02-21 10:55:04 +0100

    feat(leaderboard) : update score only if better score

--- lib/dao/leaderboard_dao.dart
+++ lib/dao/leaderboard_dao.dart
@@ -8,11 +8,27 @@ class LeaderboardDao {
       FirebaseFirestore.instance.collection("scores").get();
 
   updateUserScore(String nickname, int score) async {
-    await FirebaseFirestore.instance.collection("scores").doc(nickname).set(
-      {
-        "value": score,
-      },
-      SetOptions(merge: true),
-    );
+    DocumentSnapshot userDocument =
+        await LeaderboardDao().fetchUserScore(nickname);
+
+    if (userDocument.data() == null) {
+      await FirebaseFirestore.instance.collection("scores").doc(nickname).set(
+        {
+          "value": score,
+        },
+        SetOptions(merge: true),
+      );
+    } else {
+      Map<String, dynamic> storedScore =
+          userDocument.data() as Map<String, dynamic>;
+      if (score < storedScore["value"]) {
+        await FirebaseFirestore.instance.collection("scores").doc(nickname).set(
+          {
+            "value": score,
+          },
+          SetOptions(merge: true),
+        );
+      }
+    }
   }
 }
